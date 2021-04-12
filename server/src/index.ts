fcf82c162d8381bb7be85525ad11c51c557de94e
Adrian Nidecki <adrian.nidecki@wp.pl>
2021-04-12 18:27:32 +0200

    remove useless middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,30 +17,8 @@ const app = express();
 const RedisStore = connectRedis(session);
 const redis = new Redis(process.env.REDIS_URL);
 
-app.set("trust proxy", true);
-app.use(function (_, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
+app.set("trust proxy", 1);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", 1);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(
   session({
     name: "sid",
